a9acd8b985787323f0353adde6f16b12f284a67f
Mohammed Elguennouni <elguennouni.mohammed@gmail.com>
2019-01-01 17:49:24 +0100

    3 Commit GIT

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 	public static void main(String[] args ) {
 		for (int i=1;i<=10;i++ ) {
-			System.out.println("Hello zorld ... "+i);
+			System.out.println("Hello 2 world ... "+i);
 		}
 	}
 }
